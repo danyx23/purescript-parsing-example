779572b9b2d83986dce03ae2ce39830f22bf6ef2
Daniel Bachler <daniel@danielbachler.de>
2019-10-26 12:13:01 +0000

    Simple number parser

--- src/Main.purs
+++ src/Main.purs
@@ -1,10 +1,28 @@
 module Main where
 
-import Prelude
+import Prelude hiding (between,when)
 
+import Data.Either (Either(..))
 import Effect (Effect)
 import Effect.Console (log)
+import Text.Parsing.Parser (Parser, runParser)
+import Text.Parsing.Parser.Language (emptyDef)
+import Text.Parsing.Parser.Token (TokenParser, makeTokenParser)
+
+simpleArithmeticTokenParser :: TokenParser
+simpleArithmeticTokenParser = makeTokenParser emptyDef
+
+testValue :: String
+testValue = "100"
+
+simpleArithmeticParser :: Parser String Int
+simpleArithmeticParser = simpleArithmeticTokenParser.integer
 
 main :: Effect Unit
 main = do
-  log "🍝"
+  let parseResult = runParser testValue simpleArithmeticParser
+  case parseResult of
+    Right parsed -> do
+      log "Parsed a number!"
+    Left err -> do
+      log "Could not parse..."
