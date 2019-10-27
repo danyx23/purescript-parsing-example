2ed57f8782eb9d2c26e9cd8768d2f0e0ace0c43a
Daniel Bachler <daniel@danielbachler.de>
2019-10-27 07:30:15 +0000

    Simplify code

--- src/Main.purs
+++ src/Main.purs
@@ -2,18 +2,18 @@ module Main where
 
 import Prelude hiding (between,when)
 
+import Control.Lazy (defer)
 import Data.Either (Either(..))
+import Data.List (List(..), many)
+import Data.Symbol (SProxy(..))
 import Effect (Effect)
 import Effect.Console (log)
-import Text.Parsing.Parser (Parser, runParser, parseErrorMessage )
-import Text.Parsing.Parser.Language (emptyDef)
-import Text.Parsing.Parser.Token (TokenParser, makeTokenParser)
-import Text.Parsing.Parser.String (char, eof, string)
-import Text.Parsing.Parser.Combinators (choice, try)
 import Record.Format (format)
-import Data.Symbol (SProxy(..))
-import Control.Lazy (defer)
-import Data.List (List(..), many)
+import Text.Parsing.Parser (Parser, runParser, parseErrorMessage)
+import Text.Parsing.Parser.Combinators (choice, try)
+import Text.Parsing.Parser.Language (emptyDef)
+import Text.Parsing.Parser.String (char, eof, string, whiteSpace)
+import Text.Parsing.Parser.Token (TokenParser, makeTokenParser)
 
 
 tokenParser :: TokenParser
@@ -34,18 +34,28 @@ instance showMove :: Show Move where
   show MoveRight = "right"
 
 testValue :: String
-testValue = "left left up"
+testValue = " left left up"
+
+moveLiteralParser :: String -> Move -> Parser String Move
+moveLiteralParser literal moveValue = do
+  matchedString <- tokenParser.lexeme (string literal)
+  pure moveValue
+
+singleMoveParser :: Parser String Move
+singleMoveParser =
+  choice
+    [ moveLiteralParser "up" MoveUp
+    , moveLiteralParser "down" MoveDown
+    , moveLiteralParser "left" MoveLeft
+    , moveLiteralParser "right" MoveRight
+    ]
 
 simpleMoveParser :: Parser String (List Move)
-simpleMoveParser =
-  (many $ choice
-    [ do matchedString <- tokenParser.lexeme (string "up")
-         pure MoveUp
-    , (\ _ -> MoveDown) <$> tokenParser.lexeme (string "down")
-    , (\ _ -> MoveLeft) <$> tokenParser.lexeme (string "left")
-    , (\ _ -> MoveRight) <$> tokenParser.lexeme (string "right")
-    ])
-  <* eof
+simpleMoveParser = do
+  _ <- whiteSpace
+  moves <- many singleMoveParser
+  _ <- eof
+  pure moves
 
 interpretMoves :: List Move -> Position -> Position
 interpretMoves Nil pos = pos
